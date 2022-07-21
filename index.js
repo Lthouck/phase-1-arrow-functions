cbbcb7dec4b2e9e834ba38fafef4a3cba306f145
Lesslie Houck <Lesslie00@outlook.com>
2022-07-21 10:56:45 -0700

    commit tree 88c0e5aa30de41f17726ed1c11fa405192ca1ab7

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(){
+    const divide = 2000 / 100
+    return divide
+}
+
+const square = x => { 
+    const square = x * x
+    return square
+}
+
+const add = (a, b) => a + b
+add(3, 4)
+
